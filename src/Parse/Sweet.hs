3dbc319c5331294413dec0f4109e3b5cb39490d7
David <davidcoates95@gmail.com>
2017-12-10 14:46:04 +1100

    allow prefix negation

--- src/Parse/Sweet.hs
+++ src/Parse/Sweet.hs
@@ -1,5 +1,5 @@
 module Parse.Sweet 
-  (parse, Expr(..), Lit(..), Op(..))
+  (parse, Expr(..), Lit(..), Op(..), Tok(..))
 where
 
 import Prelude hiding (exp)
@@ -7,8 +7,8 @@ import Control.Applicative ((<|>))
 import Text.Parsec (State, parseTest, try)
 import Text.Parsec.String (Parser)
 import Text.ParserCombinators.Parsec.Char (digit)
-import Text.ParserCombinators.Parsec.Combinator (many1, eof, sepBy1)
 import Text.ParserCombinators.Parsec.Prim (many)
+import Text.ParserCombinators.Parsec.Combinator
 import qualified Text.ParserCombinators.Parsec.Prim as Prim (parse) 
 import Text.ParserCombinators.Parsec.Language (haskellDef)
 import Text.Parsec.Error (ParseError)
@@ -26,23 +26,26 @@ natural    = Token.natural    lexer
 data Lit = Integer Integer
          deriving (Show)
 
-data Op = Plus | Eq
-        deriving (Show)
+data Op = Op String
+        deriving (Show, Ord, Eq)
 
 -- data Binding = Binding String Expr
 --             deriving (Show)
 
+data Tok = TExp Expr | TOp Op | TNeg
+         deriving (Show)
+
 data Expr = Lit Lit
 --          | Let [Binding] Expr
 --          | If Expr Expr Expr
-          | OpSequence [Expr] [Op]
+          | OpSequence [Tok]
           deriving (Show)
 
-sepBy1Full :: Parser a -> Parser sep -> Parser ([a], [sep])
+sepBy1Full :: Parser [a] -> Parser [a] -> Parser [a]
 sepBy1Full a sep = do
   x <- a
-  (os, xs) <- unzip <$> many (do { y <- sep; z <- a; return (y, z) })
-  return (x:xs, os)
+  xs <- concat <$> many (do { y <- sep; z <- a; return (y ++ z) })
+  return (x ++ xs)
 
 -- instance Show (State s a) where
 --   show _ = ""
@@ -52,7 +55,7 @@ int :: Parser Expr
 int = (Lit . Integer) <$> natural
 
 op :: Parser Op
-op = ( symbol "+" >> return Plus) <|> ( symbol "==" >> return Eq )
+op = ( Op <$> symbol "+" ) <|> ( Op <$> symbol "==" )
  
 qop :: Parser Op
 qop = op
@@ -63,11 +66,9 @@ exp :: Parser Expr
 exp = infixexp
 
 infixexp :: Parser Expr
-infixexp = do
-  -- (es, os) <- sepBy1Full lexp qop
-  es <- sepBy1 lexp qop
-  let os = []
-  return (OpSequence es os)
+infixexp = OpSequence <$> sepBy1Full lexp' ((\x -> [TOp x]) <$> qop)
+    where lexp' :: Parser [Tok]
+          lexp' = do { n <- option [] (symbol "-" >> return [TNeg]); e <- lexp; return (n ++ [TExp e]) }
 
 lexp :: Parser Expr
 lexp = fexp
--- src/Parse/Unsweet.hs
+++ src/Parse/Unsweet.hs
@@ -3,17 +3,13 @@ module Parse.Unsweet
 where
 
 import qualified Parse.Sweet as Sweet
+import Parse.Sweet (Op(..), Lit(..), Tok(..))
 import Text.Parsec.Error (ParseError)
 import Text.Parsec.Prim hiding (parse)
 import Text.Parsec.Combinator
 import qualified Data.Map.Strict as Map
 import Data.Map (Map)
-
-data Lit = Integer Integer
-         deriving (Show)
-
-data Op = Plus | Eq | User String
-  deriving (Show, Ord, Eq)
+import Control.Monad (guard)
 
 -- data Binding = Binding String Expr
  --            deriving (Show)
@@ -21,46 +17,52 @@ data Op = Plus | Eq | User String
 data Expr = Lit Lit
 --          | Let Binding Expr
 --          | If Expr Expr Expr
+          | Neg Expr
           | Prim Op Expr Expr -- Eventually replace this with function application
           deriving (Show)
-
-desugarLit :: Sweet.Lit -> Lit
-desugarLit (Sweet.Integer n) = Integer n
-
-desugarOp :: Sweet.Op -> Op
-desugarOp Sweet.Plus = Plus
-desugarOp Sweet.Eq   = Eq
 
 data Assoc = Leftfix | Rightfix | Nonfix deriving Eq
 type Fixity = (Int, Assoc)
 
 opTable :: Map Op Fixity
-opTable = Map.fromList [(Plus, (6, Leftfix)), (Eq, (4, Nonfix)), (User "", (-1, Nonfix))]
+opTable = Map.fromList [(Op "+", (6, Leftfix)), (Op "==", (4, Nonfix))]
 
 desugarExpr :: Sweet.Expr -> Expr
-desugarExpr (Sweet.Lit lit) = Lit (desugarLit lit)
-desugarExpr (Sweet.OpSequence es os) = case resolve opTable (map desugarExpr es) (map desugarOp os) of Just x -> x
+desugarExpr (Sweet.Lit lit) = Lit lit
+desugarExpr (Sweet.OpSequence ts) = case resolve opTable ts of Just x -> x
 
-resolve :: Map Op Fixity -> [Expr] -> [Op] -> Maybe Expr 
-resolve fixity es os = fst <$> parse (User "":os) es
+resolve :: Map Op Fixity -> [Sweet.Tok] -> Maybe Expr
+resolve fixity ts = fst <$> parseNeg (Op "", (-1, Nonfix)) ts
   where  
-    parse :: [Op] -> [Expr] -> Maybe (Expr, ([Op], [Expr]))  
-    parse _  [e] = Just (e, ([],[]))
-    parse (op1:op2:ops) (e:es)
+    parseNeg :: (Op, Fixity) -> [Sweet.Tok] -> Maybe (Expr, [Sweet.Tok])
+    parseNeg op1 (TExp e1 : rest)
+      = parse op1 (desugarExpr e1) rest
+    parseNeg op1 (TNeg : rest)
+      = do guard (prec1 < 6)
+           (r, rest') <- parseNeg (Op "-", (6, Leftfix)) rest
+           parse op1 (Neg r) rest'
+      where
+        (s1, (prec1, fix1)) = op1
+
+    parse :: (Op, Fixity) -> Expr -> [Sweet.Tok] -> Maybe (Expr, [Sweet.Tok])
+    parse _   e1 [] = Just (e1, [])  
+    parse op1 e1 (TOp s2 : rest)  
        -- case (1): check for illegal expressions  
        | prec1 == prec2 && (fix1 /= fix2 || fix1 == Nonfix)  
        = Nothing  
  
        -- case (2): op1 and op2 should associate to the left  
        | prec1 > prec2 || (prec1 == prec2 && fix1 == Leftfix)  
-       = Just (e, (op2:ops, es))
+       = Just (e1, TOp s2 : rest)  
  
        -- case (3): op1 and op2 should associate to the right  
        | otherwise  
-       = do (r, (ops', es')) <- parse (op2:ops) es  
-            parse (op1:ops') (Prim op2 e r : es')  
-         where (prec1, fix1) = fixity Map.! op1
-               (prec2, fix2) = fixity Map.! op2 
+       = do (r,rest') <- parseNeg op2 rest  
+            parse op1 (Prim s2 e1 r) rest'
+       where  
+        (_, (prec1, fix1))  = op1
+        op2 = (s2, fixity Map.! s2)
+        (_, (prec2, fix2)) = op2
 
 parse :: String -> Either ParseError Expr
 parse s = desugarExpr <$> Sweet.parse s

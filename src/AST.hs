4dfcbbe97ebc15716988a9270e8ce6aec94eb34d
David <davidcoates95@gmail.com>
2018-01-01 23:24:44 +1100

    consisten printing of sourcepositions

--- src/AST.hs
+++ src/AST.hs
@@ -2,7 +2,8 @@
 
 module AST
   ( Error(..)
-  , SourcePos
+  , SourcePos(..)
+  , getPosition
   , indent
   , Lit(..)
   , Prim(..)
@@ -11,15 +12,23 @@ module AST
   , Annotate
   , Praxis
   , PraxisTail
+  , mapExp
   , lift
   , tagTree
+  , TreeShow(..)
   ) where
 
 import Data.Tree (Tree(..))
 import Data.Tree.Pretty (drawVerticalTree)
-import Text.Parsec.Pos (sourceName, sourceLine, sourceColumn, SourcePos)
+import Text.Parsec.Pos (sourceName, sourceLine, sourceColumn)
+import qualified Text.Parsec.Pos as Parsec (SourcePos)
 import Text.Parsec.String (Parser)
-import Text.ParserCombinators.Parsec.Prim (getPosition)
+import qualified Text.ParserCombinators.Parsec.Prim as Parsec (getPosition)
+
+newtype SourcePos = SourcePos Parsec.SourcePos
+
+instance Show SourcePos where
+  show (SourcePos p) = sourceName p ++ ":" ++ show (sourceLine p) ++ ":" ++ show (sourceColumn p)
 
 data Error a = Error { pos :: SourcePos, stage :: String,  message :: a }
 
@@ -27,8 +36,10 @@ indent :: String -> String
 indent = unlines . map ("  " ++) . lines
 
 instance Show a => Show (Error a) where
-  show e = "error in stage <<" ++ stage e ++ ">> at " ++ sourceName p ++ ":" ++ show (sourceLine p) ++ ":" ++ show (sourceColumn p) ++ " " ++ indent (show (message e))
+  show e = "error in stage <<" ++ stage e ++ ">> at " ++ show p ++ indent (show (message e))
          where p = pos e
+
+getPosition = SourcePos <$> Parsec.getPosition
 
 
 data Lit = Integer Integer
@@ -42,6 +53,14 @@ data Exp a = If (a (Exp a)) (a (Exp a)) (a (Exp a))
            | Apply (a (Exp a)) (a (Exp a))
            | Prim Prim
 
+mapExp :: (a -> b) -> Annotate a Exp -> Annotate b Exp
+mapExp f (p :< x) = f p :< mapExp' x
+  where mapExp' (If x y z)  = If (mapExp f x) (mapExp f y) (mapExp f z)
+        mapExp' (Lit l)     = Lit l
+        mapExp' (Fun s)     = Fun s
+        mapExp' (Apply a b) = Apply (mapExp f a) (mapExp f b)
+        mapExp' (Prim p)    = Prim p
+
 -- Primitive terms such as prefix negation
 data Prim = Neg
 
@@ -49,9 +68,6 @@ instance Show Prim where
   show Neg = "[-]"
 
 data Tag a b = a :< b
-
-tagTree :: Show a => (b -> Tree String) -> Tag a b -> Tree String
-tagTree f (a :< x) = let Node y bs = f x in Node (show a ++ " :< " ++ y) bs
 
 {-
 instance Functor (Tag a) where
@@ -70,22 +86,29 @@ lift f = do
   x <- f
   return (p :< x)
 
-class TreeShow a where
-  treeShow :: a -> Tree String
+tagTree :: Show a => (b -> Tree String) -> Tag a b -> Tree String
+tagTree f (a :< x) = let Node y bs = f x in Node (show a ++ " :< " ++ y) bs
 
-instance Show a => TreeShow (Annotate a Exp) where
-  treeShow = tagTree treeShow'
-    where treeShow' (If x y z)  = Node "[if]" [treeShow x, treeShow y, treeShow z]
-          treeShow' (Lit lit)   = Node (show lit) []
-          treeShow' (Fun s)     = Node s []
-          treeShow' (Prim p)    = Node (show p) []
-          treeShow' (Apply (_ :< e) x) = let (a, b) = compress e in Node a (b ++ [treeShow x])
+class TreeShow b where
+  toTreeString :: Show a => Annotate a b -> Tree String
+  showTree :: Show a => Annotate a b -> String
+  showTree = drawVerticalTree . toTreeString
 
-          compress :: Show a => Exp (Tag a) -> (String, [Tree String])
-          compress (Prim p)           = (show p, [])
-          compress (Fun s)            = (s, [])
-          compress (Apply (_ :< e) x) = let (f, y) = compress e in (f, y ++ [treeShow x])
-          compress x                  = ("$", [treeShow' x])
+instance TreeShow Exp where
+  toTreeString = treeShow
+      where treeShow :: Show a => Annotate a Exp -> Tree String
+            treeShow = tagTree treeShow'
+            treeShow' (If x y z)  = Node "[if]" [treeShow x, treeShow y, treeShow z]
+            treeShow' (Lit lit)   = Node (show lit) []
+            treeShow' (Fun s)     = Node s []
+            treeShow' (Prim p)    = Node (show p) []
+            treeShow' (Apply (_ :< e) x) = let (a, b) = compress e in Node a (b ++ [treeShow x])
 
-instance Show a => Show (Annotate a Exp) where
-  show = drawVerticalTree . treeShow
+            compress :: Show a => Exp (Tag a) -> (String, [Tree String])
+            compress (Prim p)           = (show p, [])
+            compress (Fun s)            = (s, [])
+            compress (Apply (_ :< e) x) = let (f, y) = compress e in (f, y ++ [treeShow x])
+            compress x                  = ("$", [treeShow' x])
+
+instance Show (Praxis Exp) where
+  show t = showTree t

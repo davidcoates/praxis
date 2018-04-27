6f02558699a09e9d0725992346c5b16d1cd65177
David <davidcoates95@gmail.com>
2018-04-27 13:48:48 +1000

    tokenise everything (?) except comments

--- src/Parse/Tokenise.hs
+++ src/Parse/Tokenise.hs
@@ -8,6 +8,7 @@ import Source
 import Data.Digits (unDigits)
 
 import Control.Applicative (Applicative, Alternative, liftA2, (<|>), empty)
+import Data.Foldable (asum)
 import Data.Char
 
 import Compiler
@@ -19,6 +20,9 @@ tokenise = do
   case runTokeniser atom cs of
     Left e   -> throwError e
     Right ts -> set tokens ts >> debugPrint ts
+
+
+-- // START OF NON-BACKTRACKING PARSER COMBINATORS
 
 many :: Tokeniser p -> Tokeniser [p]
 many p = liftA2 (:) (try p) (many p) <|> pure []
@@ -40,7 +44,7 @@ anyChar = satisfy (const True)
 
 oneOf :: [Char] -> Tokeniser Char
 oneOf cs = satisfy (`elem` cs)
-
+ 
 string :: String -> Tokeniser String
 string [c] = (:[]) <$> char c
 string (c:cs) = liftA2 (:) (char c) (string cs)
@@ -48,53 +52,82 @@ string (c:cs) = liftA2 (:) (char c) (string cs)
 atom :: Tokeniser Token
 atom = (whitespace *> pure Whitespace) <|> lexeme
 
--- The parsers below here all have a 'prefix' which they match without consuming on failure
+whitestuff :: Tokeniser String
+whitestuff = whitechar -- TODO <:> comment <:> ncomment
 
+-- // END OF NON-BACKTRACKING PARSER COMBINATORS
+
+
+reservedids = ["let", "in", "if", "then", "else"]
+reservedops = [":", "=", "\\", "->", "@", "=>", "|"]
 
 lexeme :: Tokeniser Token
-lexeme = qvarid <|> literal <|> special <|?> "lexeme"
+lexeme = qstuff <|> reservedid <|> reservedop <|> literal <|> special <|?> "lexeme"
 
-qvarid :: Tokeniser Token
-qvarid = qualify <$> varid <?> "qvarid" -- TODO: Qualify
-  where qualify s = QVarId (QString {qualification = [], name = s})
+modid :: Tokeniser [String]
+modid = liftA2 (:) conid (many (try (char '.' *> conid)))
+
+conid :: Tokeniser String
+conid = try (liftA2 (:) large (many small)) <?> "conid"
 
 varid :: Tokeniser String
-varid = q <?> "varid"
-  where q = p `excludes` ["if","then","else","let","in"]
-        p = liftA2 (:) (try small) (many small) -- TODO
+varid = try (some small `excludes` reservedids) <?> "varid"
+
+varsym :: Tokeniser String
+varsym = try (liftA2 (:) (symbol `exclude` ':') (many symbol) `excludes` reservedops) <?> "varsym" -- TODO exclude dashes
+
+consym :: Tokeniser String
+consym = try (liftA2 (:) (char ':') (many symbol) `excludes` reservedops) <?> "consym"
+
+qstuff :: Tokeniser Token
+qstuff = do
+  qs <- modid <|> pure []
+  let simple = fmap (\(f, s) -> f QString{qualification=[], name=s}) stuff
+  let full   = fmap (\(f, s) -> f QString{qualification=qs, name=s}) (try (char '.' *> stuff)) <|> pure (QConId QString{qualification = init qs, name = last qs})
+  if null qs then simple else full
+    where stuff :: Tokeniser (QString -> Token, String)
+          stuff = qualify QVarId varid <|> qualify QConId conid <|> qualify QVarSym varsym <|> qualify QConSym consym <|?> "varid, conid, varsym, or consym"
+          qualify f = fmap (\s -> (f, s))
+
+reservedid :: Tokeniser Token
+reservedid = ReservedId <$> asum (map (try . string) reservedids)
+
+reservedop :: Tokeniser Token
+reservedop = ReservedOp <$> asum (map (try . string) reservedops)
 
 small :: Tokeniser Char
-small = satisfy isLower <?> "small"
+small = try (satisfy isLower) <?> "small"
+
+large :: Tokeniser Char
+large = try (satisfy isUpper) <?> "large"
+
+symbol :: Tokeniser Char
+symbol = ascSymbol <|> try (satisfy isSymbol) <?> "symbol"
+
+ascSymbol :: Tokeniser Char 
+ascSymbol = try $ oneOf "!#$%&*+./<=>?@\\^|-~:"
 
 special :: Tokeniser Token
 special = Special <$> try (oneOf "(),;[]`{}") <?> "special"
 
 literal :: Tokeniser Token
-literal = integer <|> char' <|> string' <|?> "literal"
+literal = integer <|> charLit <|> stringLit <|?> "literal"
 
--- TODO This is gross
-char' :: Tokeniser Token
-char' = try (char '\'') *> (Lit . Char <$> inner) <* (char '\'' <?> "terminating '") <?> "char"
+charLit :: Tokeniser Token
+charLit = try (char '\'') *> (Lit . Char <$> inner) <* (char '\'' <?> "terminating '") <?> "char"
   where inner = anyChar `excludes` ['\\', '\''] -- TODO
 
-string' :: Tokeniser Token
-string' = try (char '\"') *> (Lit . String <$> inner) <* char '\"' <?> "string"
+stringLit :: Tokeniser Token
+stringLit = try (char '\"') *> (Lit . String <$> inner) <* char '\"' <?> "string"
   where inner = many (anyChar `excludes` ['\\','\"']) -- TODO
 
 integer :: Tokeniser Token
 integer = Lit . Int <$> decimal <?> "integer"
-
-decimal :: Tokeniser Int
-decimal = unDigits 10 <$> liftA2 (:) (try digit) (many digit)
-
-digit :: Tokeniser Int
-digit = read . (:[]) <$> satisfy isDigit
+  where decimal :: Tokeniser Int
+        decimal = read <$> some (try (satisfy isDigit))
 
 whitespace :: Tokeniser String
 whitespace = try (concat <$> some whitestuff) <?> "whitespace"
-
-whitestuff :: Tokeniser String
-whitestuff = whitechar -- <:> comment <:> ncomment
 
 whitechar :: Tokeniser String
 whitechar = try newline <|> ((:[]) <$> satisfy isSpace)

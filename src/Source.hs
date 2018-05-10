525603891cdc9d927cc56d8e2cb6c81892d67ef9
David <davidcoates95@gmail.com>
2018-05-10 15:47:05 +1000

    show phantom source as ?

--- src/Source.hs
+++ src/Source.hs
@@ -7,13 +7,13 @@ module Source
 data Pos = Pos { line :: Int, column :: Int }
 
 data Source = Source { start :: Pos, end :: Pos, spelling :: String }
-            | Phantom -- ^Used for phantom tokens e.g., layout tokens inserted by the tokeniser 
+            | Phantom -- ^Used for phantom tokens e.g., layout tokens inserted by the tokeniser
 
 instance Show Pos where
   show p = show (line p) ++ ":" ++ show (column p)
 
 showSource :: ((Pos, Pos, String) -> String) -> Source -> String
-showSource _ Phantom = "\x26A1\x26A1\x26A1PHANTOM\x26A1\x26A1\x26A1"
+showSource _ Phantom = "?" -- "\x26A1\x26A1\x26A1PHANTOM\x26A1\x26A1\x26A1"
 showSource f s       = f (start s, end s, "\x25B7" ++ spelling s ++ "\x25C1")
 
 instance Show Source where

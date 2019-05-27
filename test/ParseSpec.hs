c331d616686c694515136a16c4d061f9c2ee03cc
david <davidcoates95@gmail.com>
2019-05-27 17:44:50 +1000

    another parse test

--- test/ParseSpec.hs
+++ test/ParseSpec.hs
@@ -18,22 +18,29 @@ exps =
   , "f x 1 + g y * z" `matches` "((f x) 1) + ((g y) * z)"
   , "f x + f y + f z" `matches` "((f x) + (f y)) + (f z)" ]
 
-programs =
-  [
-  ]
-
 tys =
   [ "Int -> Int -> Int" `matches` "Int -> (Int -> Int)"
+  , "A B C" `matches` "(A B) C"
+  , "Maybe (Maybe a) -> Maybe b" `matches` "(Maybe (Maybe a)) -> (Maybe b)"
+  , "[A, B] -> C [D, E]" `matches` "[A, B] -> (C [D, E])"
+  ]
+
+programs =
+  [ " fac : Int -> Int   \n\
+    \ fac = cases        \n\
+    \ 0 -> 1             \n\
+    \ n -> n * fac (n -1)"
+    `matches` "{ fac : Int -> Int; fac = cases { 0 -> 1; n -> n * (fac (n - 1)) } }"
   ]
 
 exp :: String -> Parsed Exp
 exp s = runInternal emptyState (parse s)
 
-program :: String -> Parsed Program
-program s = runInternal emptyState (parse s)
-
 ty :: String -> Parsed Type
 ty s = runInternal emptyState (parse s)
+
+program :: String -> Parsed Program
+program s = runInternal emptyState (parse s)
 
 spec :: Spec
 spec = do

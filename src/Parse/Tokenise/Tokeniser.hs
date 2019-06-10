741e0a251539bc162d3c5cfa9f872505b236a9b3
david <davidcoates95@gmail.com>
2019-06-10 16:19:23 +1000

    formatting

--- src/Parse/Tokenise/Tokeniser.hs
+++ src/Parse/Tokenise/Tokeniser.hs
@@ -35,23 +35,23 @@ instance Alternative Tokeniser where
   Tokeniser a <|> Tokeniser b = Tokeniser (a <|> b)
 
 run :: Pretty a => Tokeniser (Maybe a) -> String -> Praxis [Sourced a]
-run (Tokeniser t) cs = all (sourced cs)
-  where all [] = pure []
-        all cs = case Parser.run t cs of
-          Left e                  -> Praxis.throw $ e
-          Right (s :< Just x, cs) -> ((:) <$> pure (s :< x) <*> all cs)
-          Right (_, cs)           -> all cs
+run (Tokeniser t) cs = all (sourced cs) where
+  all [] = pure []
+  all cs = case Parser.run t cs of
+    Left e                  -> Praxis.throw $ e
+    Right (s :< Just x, cs) -> ((:) <$> pure (s :< x) <*> all cs)
+    Right (_, cs)           -> all cs
 
 sourced :: String -> [Sourced Char]
-sourced = sourced' Pos { line = 1, column = 1 }
-  where sourced' _     [] = []
-        sourced' p (c:cs) = make p c : sourced' (advance c p) cs
-        make p c = Source { start = p, end = p } :< c
-        advance :: Char -> Pos -> Pos
-        advance '\t' p = p { column = math (column p) }
-          where math = (+ 1) . (* 8) . (+ 1) . (`div` 8) . subtract 1
-        advance '\n' p = Pos { line = line p + 1, column = 1 }
-        advance _    p = p { column = column p + 1 }
+sourced = sourced' Pos { line = 1, column = 1 } where
+  sourced' _     [] = []
+  sourced' p (c:cs) = make p c : sourced' (advance c p) cs
+  make p c = Source { start = p, end = p } :< c
+  advance :: Char -> Pos -> Pos
+  advance c p = case c of
+    '\t' -> p { column = math (column p) } where math = (+ 1) . (* 8) . (+ 1) . (`div` 8) . subtract 1
+    '\n' -> Pos { line = line p + 1, column = 1 }
+    _    -> p { column = column p + 1 }
 
 satisfies :: Int -> (String -> Bool) -> Tokeniser ()
 satisfies i f = Tokeniser (Parser.satisfies i (f . map (view value)) *> pure (pure ()))
@@ -60,9 +60,9 @@ satisfy :: (Char -> Bool) -> Tokeniser ()
 satisfy p = satisfies 1 (\[c] -> p c)
 
 matches :: Int -> (String -> Bool) -> Tokeniser String
-matches n p = satisfies n p *> consumes n
-  where consumes 0 = pure ""
-        consumes n = (:) <$> consume <*> consumes (n - 1)
+matches n p = satisfies n p *> consumes n where
+  consumes 0 = pure ""
+  consumes n = (:) <$> consume <*> consumes (n - 1)
 
 match :: (Char -> Bool) -> Tokeniser Char
 match p = Tokeniser $ Parser.match (p . view value)

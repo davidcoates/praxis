b8943c922aedfdd694e7cb52e2a73cb6a453cf18
David <davidcoates95@gmail.com>
2019-09-13 17:37:54 +1000

    remove AEnv

--- src/Env/LEnv.hs
+++ src/Env/LEnv.hs
@@ -17,55 +17,58 @@ module Env.LEnv
   )
 where
 
-import           Env.AEnv      (AEnv (..))
-import qualified Env.AEnv      as AEnv
+import           Env.Env      (Env (..))
+import qualified Env.Env      as Env
 
 import           Control.Arrow (second)
 import           Data.List     (intercalate)
 import           Prelude       hiding (lookup)
 import qualified Prelude       (lookup)
 
-data LEnv a b = LEnv (AEnv a b) [AEnv a b]
+data LEnv a b = LEnv (Env a (Bool, b)) [Env a (Bool, b)]
 
 deriving instance Foldable (LEnv a)
 deriving instance Traversable (LEnv a)
 
 instance Functor (LEnv a) where
-  fmap f (LEnv l ls) = LEnv (fmap f l) (map (fmap f) ls)
+  fmap f (LEnv l ls) = LEnv (fmap f' l) (map (fmap f') ls) where
+    f' (u, x) = (u, f x)
 
 fromList :: [(a, b)] -> LEnv a b
-fromList vs = LEnv (AEnv.fromList vs) []
+fromList vs = LEnv (Env.fromList (map (\(a, b) -> (a, (False, b))) vs)) []
 
 instance (Show a, Show b) => Show (LEnv a b) where
-  show (LEnv l ls) = "[" ++ intercalate "|" (map show (l:ls)) ++ " ]"
+  show (LEnv l ls) = intercalate "|" (map show' (l:ls)) where
+    show' (Env l) = "[" ++ intercalate ", " (map (\(a,(u,b)) -> show a ++ (if u then " :* " else " :o ") ++ show b) l) ++ " ]"
 
 elim :: LEnv a b -> LEnv a b
-elim (LEnv l ls) = LEnv (AEnv.elim l) ls
+elim (LEnv l ls) = LEnv (Env.elim l) ls
 
 elimN :: Int -> LEnv a b -> LEnv a b
-elimN n (LEnv l ls) = LEnv (AEnv.elimN n l) ls
+elimN n (LEnv l ls) = LEnv (Env.elimN n l) ls
 
 intro :: a -> b -> LEnv a b -> LEnv a b
-intro a b (LEnv l ls) = LEnv (AEnv.intro a b l) ls
+intro a b (LEnv l ls) = LEnv (Env.intro a (False, b) l) ls
 
 mark :: Eq a => a -> LEnv a b -> LEnv a b
 mark x (LEnv l ls) = let (m:ms) = f (l:ls) in LEnv m ms
-  where f (l:ls) = case AEnv.lookup x l of
-          Just _  -> AEnv.mark x l : ls
+  where f (l:ls) = case Env.lookup x l of
+          Just _  -> Env.adjust (\(_, b) -> (True, b)) x l : ls
           Nothing -> l : f ls
 
 lookup :: Eq a => a -> LEnv a b -> Maybe (Bool, Bool, b)
 lookup x (LEnv l ls) = f (l:ls) False
   where f     [] _ = Nothing
-        f (l:ls) c = case AEnv.lookup x l of
+        f (l:ls) c = case Env.lookup x l of
           Just (u, v) -> Just (c, u, v)
           Nothing     -> f ls True
 
 join :: LEnv a b -> LEnv a b -> LEnv a b
-join (LEnv l1 l1s) (LEnv l2 l2s) = LEnv (AEnv.join l1 l2) (zipWith AEnv.join l1s l2s)
+join (LEnv l1 l1s) (LEnv l2 l2s) = LEnv (join' l1 l2) (zipWith join' l1s l2s) where
+  join' (Env l1) (Env l2) = Env (zipWith (\(x,(xu,xt)) (y,(yu,yt)) -> (x,(xu || yu, xt))) l1 l2)
 
 push :: LEnv a b -> LEnv a b
-push (LEnv l ls) = LEnv (AEnv.fromList []) (l:ls)
+push (LEnv l ls) = LEnv (Env.fromList []) (l:ls)
 
 pop :: LEnv a b -> LEnv a b
 pop (LEnv _ (l:ls)) = LEnv l ls

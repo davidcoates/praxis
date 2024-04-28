ed2191138f6094c73600946913367e4f7836309e
David <davidcoates95@gmail.com>
2024-04-28 17:06:47 +1000

    WIP llvm codegen: Let's get cracking!

--- src/Translate.hs
+++ src/Translate.hs
@@ -8,13 +8,22 @@ import           Praxis
 import           Stage
 import           Term
 
+import qualified LLVM.Codegen as LLVM
+import qualified Data.Text as Text
+
 
 data Mode = NoPrelude | Prelude | PreludeWithMain
 
+
+type ModuleBuilder = LLVM.ModuleBuilderT Praxis
+
+type IRBuilder = LLVM.IRBuilderT ModuleBuilder
+
+-- TODO return text
 runProgram :: Mode -> Annotated Program -> Praxis String
 runProgram mode program = save stage $ do
   stage .= Translate
-  program <- translateProgram program
+  program <- (Text.unpack . LLVM.ppllvm) <$> LLVM.runModuleBuilderT (translateProgram program)
   display program `ifFlag` debug
   return program
 {-
@@ -26,5 +35,32 @@ runProgram mode program = save stage $ do
 -}
 
 
-translateProgram :: Annotated Program -> Praxis String
-translateProgram (_ :< Program decls) = return "TODO"
+translateProgram :: Annotated Program -> ModuleBuilder ()
+translateProgram (_ :< Program decls) = mapM_ translateDecl decls
+
+translateDecl :: Annotated Decl -> ModuleBuilder LLVM.Operand
+translateDecl (_ :< decl) = return undefined
+
+translateExp :: Annotated Exp -> ModuleBuilder LLVM.Operand
+translateExp (_ :< exp) = return undefined
+
+translateType :: Annotated Type -> ModuleBuilder LLVM.Type
+translateType (_ :< ty) = case ty of
+
+  TyCon n -> case n of
+    "Bool"  -> return LLVM.i1
+    "Char"  -> return LLVM.i32
+    "I8"    -> return LLVM.i8
+    "I16"   -> return LLVM.i16
+    "I32"   -> return LLVM.i32
+    "I64"   -> return LLVM.i64
+    "ISize" -> return LLVM.i64
+    "U8"    -> return LLVM.i8
+    "U16"   -> return LLVM.i16
+    "U32"   -> return LLVM.i32
+    "U64"   -> return LLVM.i64
+    "USize" -> return LLVM.i64
+    _       -> error "TODO"
+
+
+  
